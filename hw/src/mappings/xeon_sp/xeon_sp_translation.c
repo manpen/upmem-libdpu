b6905af2be0b46faa17c772a9e97f452078b59c6
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-03-21 10:19:29 +0000

    Replace _mm512_i32gather_epi32
    All AVX gather/scatter operations are incrediably expensive (Intel
    reports a latency of 30 and a CPI of 10) for this operation. Luckily,
    in `byte_interleave_avx512` it is only used to permute 32bit words.
    Thus, we can replace it with a loadu/permute sequence.

--- hw/src/mappings/xeon_sp/xeon_sp_translation.c
+++ hw/src/mappings/xeon_sp/xeon_sp_translation.c
@@ -151,25 +151,30 @@ byte_interleave_avx2(uint64_t *input, uint64_t *output)
 void
 byte_interleave_avx512(uint64_t *input, uint64_t *output, bool use_stream)
 {
-    __m512i mask;
+    __m512i load = _mm512_loadu_epi32(input);
 
-    mask = _mm512_set_epi64(0x0f0b07030e0a0602ULL,
-        0x0d0905010c080400ULL,
+    // LEVEL 0
+    __m512i vindex =
+            _mm512_setr_epi32(0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15);
+    __m512i gathered = _mm512_permutexvar_epi32(vindex, load);
 
-        0x0f0b07030e0a0602ULL,
-        0x0d0905010c080400ULL,
+    // LEVEL 1
+    __m512i mask = _mm512_set_epi64(0x0f0b07030e0a0602ULL,
+                            0x0d0905010c080400ULL,
 
-        0x0f0b07030e0a0602ULL,
-        0x0d0905010c080400ULL,
+                            0x0f0b07030e0a0602ULL,
+                            0x0d0905010c080400ULL,
 
-        0x0f0b07030e0a0602ULL,
-        0x0d0905010c080400ULL);
+                            0x0f0b07030e0a0602ULL,
+                            0x0d0905010c080400ULL,
 
-    __m512i vindex = _mm512_setr_epi32(0, 8, 16, 24, 32, 40, 48, 56, 4, 12, 20, 28, 36, 44, 52, 60);
+                            0x0f0b07030e0a0602ULL,
+                            0x0d0905010c080400ULL);
+
+    __m512i transpose = _mm512_shuffle_epi8(gathered, mask);
+
+    // LEVEL 2
     __m512i perm = _mm512_setr_epi32(0, 4, 1, 5, 2, 6, 3, 7, 8, 12, 9, 13, 10, 14, 11, 15);
-
-    __m512i load = _mm512_i32gather_epi32(vindex, input, 1);
-    __m512i transpose = _mm512_shuffle_epi8(load, mask);
     __m512i final = _mm512_permutexvar_epi32(perm, transpose);
 
     if (use_stream) {
